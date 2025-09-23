804cceefe34ec2e1ac9e67540ea03f1507d9ee40
SARANrit <sd2729@g.rit.edu>
2025-09-23 13:52:28 +0400

    abcdefg

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+def hello():
+    name = input("Enter name: ")
+    print("hello", name)
+
+hello()
+
+def box():
+    print("*****")
+    print("*   *")
+    print("*****")
+
+box()
+
+def bday():
+    print("Januray 15th, 2008")
+
+bday()
+
+def days_of_week():
+    print("Sunday")
+
+days_of_week()
